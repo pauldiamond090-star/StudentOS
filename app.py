176927685756c7c9ca3d92e44292515e48bf3673
pauldiamond090-star <pauldiamond090@gmail.com>
2026-05-11 06:51:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -10,4 +12,5 @@ def home():
     """
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
