f54603b81589732976f62af1f75a4ee3fd20bfe6
pauldiamond090-star <pauldiamond090@gmail.com>
2026-05-11 13:04:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return "<h1>Login Page</h1>"
+
+@app.route("/register")
+def register():
+    return "<h1>Register Page</h1>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
