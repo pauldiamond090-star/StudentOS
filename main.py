f012a8a3edae1f302bfd76a2c651b9337f1e7d70
pauldiamond090-star <pauldiamond090@gmail.com>
2026-05-12 11:30:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-# Home Page
-@app.route("/")
-def home():
+@app.route("/splash")
+def splash():
+    return render_template("splash.html")
+
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        return redirect("/register")
+
     return render_template("index.html")
 
-# Login Page
-@app.route("/login")
-def login():
-    return render_template("login.html")
 
-# Register Page
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+
+    if request.method == "POST":
+        return "Registration Successful"
+
     return render_template("register.html")
 
-# Student Dashboard
-@app.route("/student-dashboard")
-def student_dashboard():
-    return render_template("Student_dashboard.html")
-
-# Teacher Dashboard
-@app.route("/teacher-dashboard")
-def teacher_dashboard():
-    return render_template("teacher_dashboard.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
